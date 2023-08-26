374fe2519b67ee77d52727e531f0160db7e4d480
sanjeev01811 <adityasanjeev1996@gmail.com>
2023-08-26 12:45:48 +0530

    adding

--- provider.tf
+++ provider.tf
@@ -7,11 +7,17 @@ terraform {
   }
 }
 
+# provider "aws" {
+#   # Configuration options
+#   region     = "us-east-1"
+#   profile = "your_aws_cli_profile"
+# }
+
 provider "aws" {
-  # Configuration options
-  region     = "us-east-1"
+  region  = "us-east-1"
   profile = "your_aws_cli_profile"
 }
+
 
 #export AWS_ACCESS_KEY_ID=your_access_key
 #export AWS_SECRET_ACCESS_KEY=your_secret_key

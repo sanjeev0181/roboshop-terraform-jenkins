8c2a0a81c36de4676fe63c5870b271eeecb253a9
sanjeev01811 <adityasanjeev1996@gmail.com>
2023-08-28 15:38:46 +0530

    adding the provisioner in remote-exec

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-053b0d53c279acc90" # data.aws_ami.ubuntu.image_id
   instance_type =  var.instance_type # "t2.micro"
-  key_name = "terraform.pem"
+  key_name = "terraform"
   # vpc_security_group_ids - Associated security groups in a non-default VPC.
   vpc_security_group_ids = [ data.aws_security_group.selected.id ]
   tags = {
